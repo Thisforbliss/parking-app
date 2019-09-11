22bd3030ff628931e11b66d2a411a79a6c7510ec
Malcolm Leake <echelon1089@gmail.com>
2019-09-11 21:39:02 +0000

    First commit

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,37 @@
+class ContentsController < ApplicationController
+
+  # GET: /contents
+  get "/contents" do
+    erb :"/contents/index.html"
+  end
+
+  # GET: /contents/new
+  get "/contents/new" do
+    erb :"/contents/new.html"
+  end
+
+  # POST: /contents
+  post "/contents" do
+    redirect "/contents"
+  end
+
+  # GET: /contents/5
+  get "/contents/:id" do
+    erb :"/contents/show.html"
+  end
+
+  # GET: /contents/5/edit
+  get "/contents/:id/edit" do
+    erb :"/contents/edit.html"
+  end
+
+  # PATCH: /contents/5
+  patch "/contents/:id" do
+    redirect "/contents/:id"
+  end
+
+  # DELETE: /contents/5/delete
+  delete "/contents/:id/delete" do
+    redirect "/contents"
+  end
+end
