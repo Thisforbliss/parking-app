a44bcf564863440eb5c9e3928733aa42a493c080
Malcolm Leake <echelon1089@gmail.com>
2019-09-19 22:00:22 +0000

    figure out delete looping problem

--- app/controllers/helpers.rb
+++ app/controllers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
   def self.current_user(session_hash)
-    @driver = Driver.find(session_hash[:user_id]) if session_hash=[:user_id]
+    @driver = Driver.find(session_hash[:driver_id]) if session_hash=[:driver_id]
   end
 
   def self.is_logged_in?(session_hash)
-    !!session_hash[:user_id]
+    !!session_hash[:driver_id]
   end
 
 end
